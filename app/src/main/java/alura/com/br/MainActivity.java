4127c41abc10b8167fc3084f89e12e34ba098828
Priscila Barros <priscila.saldanha.barros@gmail.com>
2022-02-22 12:14:45 -0400

    adição de Toast

--- app/src/main/java/alura/com/br/MainActivity.java
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -1,7 +1,17 @@
 package alura.com.br;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Bono Vox ", Toast.LENGTH_LONG).show();
+    }
 
 }
