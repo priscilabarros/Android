6ebe436ecf62fd94bf81aaebc32a2a72e7b9ebc8
Priscila Barros <priscila.saldanha.barros@gmail.com>
2022-02-23 12:14:21 -0400

    Adição de List View e um povoamento de lista estatico e dinamico

--- app/src/main/java/alura/com/br/MainActivity.java
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -1,11 +1,18 @@
 package alura.com.br;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     
@@ -13,6 +20,27 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList(
+                "Bruce Wayne","Diana Themyscera","Clark Kent","John Stuart","Barry Allen"));
+
+        //Povoação dinamica para a Lista
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
+
+        /* Povoação estatica para a Lista
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));*/
+
+
     }
 
 }
